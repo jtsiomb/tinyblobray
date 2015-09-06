536657613a40415ba6fbba32b67c2bc3584e5223
John Tsiombikas <nuclear@member.fsf.org>
2015-09-06 05:54:58 +0300

    floating point stuff

--- test.asm
+++ test.asm
@@ -8,15 +8,23 @@
 %define VMEM_WIN_SIZE	65536
 	section .data
 f255:	dd 255.0
+f160:	dd 160.0
+f100:	dd 100.0
+faspect: dd 1.333333
 
 	section .bss
 winoffs: resd 1
 px:	resd 1
 py:	resd 1
 tmp:	resd 1
+rdir:	resd 3
+rpos:	resd 3
 	
 	section .text
 main:
+	mov eax, cr0		; Start probe, get CR0
+	and eax, 0fffffff3h	; clear TS and EM to force fpu access
+	mov cr0, eax		; store control word
 	finit
 
 	mov ax, 4f02h	; set video mode
@@ -41,35 +49,28 @@ xloop:
 	mov [px], eax
 	xor eax, eax
 
-	;fild dword [py]
-	;fild dword [px]
-	;call calc_pixel	; args: x(0), y(1). returns rgb(0,1,2)
+	fild dword [py]
+	fild dword [px]
+	call calc_pixel	; args: x(0), y(1). returns rgb(0,1,2)
 	; red result
-	;fmul dword [f255]
-	fld dword [f255]
-	fistp dword [tmp]
-	fwait
-	mov [tmp], eax
+	fmul dword [f255]
+	fistp dword [ds:tmp]
+	mov eax, [tmp]
 	shl ax, 8
 	and ax, 0f800h
 	; green result
-	;fmul dword [f255]
-	;fld dword [f255]
-	;fistp dword [tmp]
-	fwait
-	;mov [tmp], ebx
-	;shl bx, 3
-	;and bx, 7e0h
-	;or ax, bx
+	fmul dword [f255]
+	fistp dword [tmp]
+	mov ebx, [tmp]
+	shl bx, 3
+	and bx, 7e0h
+	or ax, bx
 	; blue result
-	;fldz
-	;fmul dword [f255]
-	;fistp dword [tmp]
-	fwait
-	;mov [tmp], ebx
-	;shr bx, 3
-	;or ax, bx
-	;mov ax, 0f800h
+	fmul dword [f255]
+	fistp dword [tmp]
+	mov ebx, [tmp]
+	shr bx, 3
+	or ax, bx
 
 	mov [es:di], ax		; write packed pixel
 	inc di
@@ -128,9 +129,62 @@ set_vmem_win:
 
 ; calc_pixel(x [st0], y [st1]) -> r [st0], g [st1], b [st2]
 calc_pixel:
-	ffree st0
-	ffree st1
-	fldz		; blue = 0
-	fld1		; green = 1
-	fld1		; red = 1
+	call calc_prim_dir
+	ret
+
+; calc_prim_dir(x [st0], y [st1]): sets global rdir vector
+calc_prim_dir:
+	; x = 1.3333 * (px / 160.0 - 1.0)
+	fdiv dword [f160]	; st0 <- px / 160.0
+	fld1
+	fsubr			; st0 <- st0 - 1.0
+	fmul dword [faspect]	; st0 <- st0 * aspect
+	fxch			; exchange with st1 to work on py now
+	; y = 1.0 - (py / 100.0)
+	fdiv dword [f100]	; st0 <- py / 100.0
+	fld1
+	fsub			; st0 <- 1.0 - st0
+	; z = 1.0 / tan(50deg / 2.0) ~ 2.14... ~ PI - 1
+	fld1
+	fldpi
+	fsub
+	; we end up with st {z, y, x}
+	fxch st2		; put them in the correct order
+	call normalize
+	fstp dword [rdir]	; store x
+	fstp dword [rdir + 4]	; store y
+	fstp dword [rdir + 8]	; store z
+	ret
+
+; length(x [st0], y [st1], z [st2]): returns length in st0
+length:
+	fld st0
+	fmul		; square x {x*x, y, z}
+	fxch
+	fld st0
+	fmul		; square y {y*y, x*x, z}
+	fadd		; st0 <- x*x+y*y (st1 is z now)
+	fxch
+	fld st0
+	fmul		; square z {z*z, y*y+x*x}
+	fadd		; st0 <- x*x+y*y+z*z
+	fsqrt
+	ret
+
+; normalize(x [st0], y [st1], z [st2]): normalize in place
+normalize:
+	; push a copy of the vector to work with
+	fld st2		; push z
+	fld st2		; push y
+	fld st2		; push x
+	call length	; now st{len, x, y, z}
+	fld1
+	fdiv		; {1/len, x, y, z}
+	fxch st3	; {z, x, y, 1/len}
+	fmul st3
+	fxch st2	; {y, x, z/len, 1/len}
+	fmul st3
+	fxch st1	; {x, y/len, z/len, 1/len}
+	fmul st3
+	ffree st3
 	ret
